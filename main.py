940f5690ff2f1f632201a14b3420a54084a4b53f
unknown <guivilatoro@gmail.com>
2026-04-01 22:50:43 -0400

    implemented the reports page and created the base for the professor's report

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from presentation.terminal import Terminal
 
 if __name__ == "__main__":
     terminal = Terminal()
-    print("test")
-    print(terminal.running)
+    
     while (terminal.running):
         val = terminal.currentMenu.render()
 
---
+++ services/SReport.py
@@ -0,0 +1,27 @@
+
+
+
+
+class ReportService:
+    def StudentReport(self, id: int):
+        print("command to be implemented")
+        print(id)
+        return (0, "Success")
+    
+    
+    def ProfessorReport(self, id : int):
+        
+        print("command to be implemented")
+        print(id)
+        return (0, "Success")
+    
+
+
+
+
+
+    
+
+
+
+
